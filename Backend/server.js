ed6a318e6ad3aa648a1f46533b0da1287bddd197
John Leo D. Echevaria <109233177+AkunoCode@users.noreply.github.com>
2023-08-27 16:05:59 +0800

    added cors on the backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose')
 const workOutRoutes = require('./Routes/workouts')
+const cors = require('cors')
 
 // express app
 const app = express();
 
 // MIDDLEWARES
 app.use(express.json())
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(`${req.method} Request from ${req.path}`)
