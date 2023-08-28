86ff767f4a1aba63aaeb3fae2991b67c872fe5c2
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-08-28 19:22:00 +0800

    added formatting and functions to workout details

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,27 @@
+import axios from "axios"
+import { useWorkoutContext } from "../hooks/useWorkoutContext"
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const URL_PATH = 'http://localhost:4000/api/workouts/'
+
 const WorkoutDetails = ({ workout }) => {
+
+    const { dispatch } = useWorkoutContext()
+
+    const handleClick = async () => {
+        const response = await axios.delete(`${URL_PATH}${workout._id}`)
+        if (response.status === 200) {
+            dispatch({ type: "DELETE-WORKOUT", payload: response.data })
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <h4><strong>Load (kg):</strong> {workout.load}</h4>
             <h4><strong>Reps:</strong> {workout.load}</h4>
-            <p>{workout.createdAt}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 }
