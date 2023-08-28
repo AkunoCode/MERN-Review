8b7eb5307b3c390c47ec53e93ba1ebdc4f7c78d6
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-08-28 19:21:25 +0800

    added error condition for POST

--- Backend/Controllers/workoutController.js
+++ Backend/Controllers/workoutController.js
@@ -46,6 +46,23 @@ If successful, it will return a status of 200 and the documents. Otherwise, a st
 */
 const createWorkout = async (req, res) => {
     const { title, reps, load } = req.body
+
+    //Check if there are empty fields
+    let emptyFields = []
+    if (!title) {
+        emptyFields.push('title')
+    }
+    if (!reps) {
+        emptyFields.push('reps')
+    }
+    if (!load) {
+        emptyFields.push('load')
+    }
+
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
+    }
+
     try {
         const workout = await Workout.create({ title, load, reps })
         res.status(200).json(workout)
